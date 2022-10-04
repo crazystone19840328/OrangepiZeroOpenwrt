f10ab22234300b44762cd5bd9f0b1dada348533e
crazystone19840328 <39001248+crazystone19840328@users.noreply.github.com>
2022-10-04 17:03:35 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,4 +11,4 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.123.2/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.1.234/g' package/base-files/files/bin/config_generate
